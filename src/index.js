afc2ef7a9ebaff8b4e67b7e5530a569667cba6ab
KaizJava_io <phunlhce150297@fpt.edu.vn>
2022-03-11 15:30:10 +0700

    update port

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ app.use("/user", userRoute);
 app.use("/plant", plantRoute);
 
 //connect port
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`server started on port http://localhost:${PORT}`)
 );
